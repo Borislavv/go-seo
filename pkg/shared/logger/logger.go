50710c1d8e5dceac433025a8d0f4246871bfe204
Boris <glazunov2142@icloud.com>
2024-05-19 15:18:55 +0300

    feat: implemented logrus logger and wrapped by interface.

---
+++ pkg/shared/logger/logger.go
@@ -0,0 +1,19 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func NewLogger() (logger *logrus.Logger, closeFunc func()) {
+	cfg := Load()
+	lgr := logrus.New()
+	lgr.SetLevel(cfg.GetLevel())
+
+	lgr.SetFormatter(cfg.GetFormat())
+	lgr.SetReportCaller(true)
+
+	output := cfg.GetOutput()
+	lgr.SetOutput(output)
+
+	return lgr, func() { _ = output.Close() }
+}

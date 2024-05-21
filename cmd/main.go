74a792c3d543d8f200ecf56ecee84d3057750241
Boris <glazunov2142@icloud.com>
2024-05-21 15:28:27 +0300

    feat: improved main.

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"context"
 	"github.com/Borislavv/go-seo/pkg/shared/logger"
 	"github.com/Borislavv/go-seo/pkg/shared/server"
+	"github.com/Borislavv/go-seo/pkg/shared/shutdown"
+	"sync"
 )
 
 func main() {
+	wg := &sync.WaitGroup{}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
 	lgr, closeFunc := logger.NewLogger()
 	defer closeFunc()
 
-	lgr.Info("Hello world from logger by interface")
-
 	server.
 		NewHTTP(lgr).
-		ListenAndServe()
+		ListenAndServe(ctx, wg)
+
+	shutdown.
+		NewGraceful(cancel).
+		ListenAndCancel()
+
+	wg.Wait()
 }

3447c08f65955985deb01713ce2015d2ef5e90ee
Boris <glazunov2142@icloud.com>
2024-05-22 17:29:20 +0300

    feat: updated http server.

--- pkg/shared/server/http.go
+++ pkg/shared/server/http.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"errors"
-	"github.com/Borislavv/go-seo/internal/pagedata/infrastructure/api/v1/http/controller"
 	"github.com/Borislavv/go-seo/pkg/shared/logger"
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
@@ -12,24 +11,22 @@ import (
 )
 
 type HTTP struct {
+	server *fasthttp.Server
+	config *Config
 	logger logger.Logger
 }
 
-func NewHTTP(logger logger.Logger) *HTTP {
-	return &HTTP{
-		logger: logger,
-	}
+func NewHTTP(logger logger.Logger, controllers []HttpController) *HTTP {
+	s := &HTTP{logger: logger, config: new(Config).Load()}
+	s.init(s.buildRouter(controllers))
+	return s
 }
 
 func (s *HTTP) ListenAndServe(ctx context.Context, wg *sync.WaitGroup) {
-	r := router.New()
-	controller.NewPagedataGetController(s.logger).AddRoute(r)
-	server := fasthttp.Server{Handler: r.Handler}
-
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		if err := server.ListenAndServe(":8087"); err != nil {
+		if err := s.server.ListenAndServe(s.config.Port); err != nil {
 			s.logger.Error(err)
 		}
 	}()
@@ -43,15 +40,29 @@ func (s *HTTP) ListenAndServe(ctx context.Context, wg *sync.WaitGroup) {
 
 		<-ctx.Done()
 
-		sctx, cancel := context.WithTimeout(ctx, time.Second*15)
+		sctx, cancel := context.WithTimeout(ctx, time.Duration(s.config.ShutDownTimeoutSeconds))
 		defer cancel()
 
-		if err := server.ShutdownWithContext(sctx); err != nil {
+		if err := s.server.ShutdownWithContext(sctx); err != nil {
 			if errors.Is(err, context.Canceled) {
+				s.logger.Info(err)
+			} else {
 				s.logger.Error(err)
 			}
 		}
 	}()
 
-	s.logger.Info("http server was started")
+	s.logger.Infof("http server was started on port http://0.0.0.0%v", s.config.Port)
+}
+
+func (s *HTTP) buildRouter(controllers []HttpController) *router.Router {
+	r := router.New()
+	for _, controller := range controllers {
+		controller.AddRoute(r)
+	}
+	return r
+}
+
+func (s *HTTP) init(r *router.Router) {
+	s.server = &fasthttp.Server{Handler: r.Handler}
 }

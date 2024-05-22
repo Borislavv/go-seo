0ee0bc1d95bad1f9f97df9946ea5c6890e25c217
Boris <glazunov2142@icloud.com>
2024-05-22 17:28:14 +0300

    feat: new Config into the shared.

---
+++ pkg/shared/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	ProdEnv = "prod"
+	DevEnv  = "dev"
+	TestEnv = "test"
+)
+
+var cfg *Config
+
+type Config struct {
+	appEnv string `env:"APP_ENV"`
+}
+
+func Load() {
+	cfg = new(Config)
+	cfg.appEnv = os.Getenv("APP_ENV")
+	if cfg.appEnv == "" {
+		panic(fmt.Sprintf("APP_ENV environment variable must be defined"))
+	}
+}
+
+func Get() *Config {
+	return cfg
+}
+
+func (c *Config) IsProd() bool {
+	return c.appEnv == ProdEnv
+}
+
+func (c *Config) IsDev() bool {
+	return c.appEnv == DevEnv
+}
+
+func (c *Config) IsTest() bool {
+	return c.appEnv == TestEnv
+}

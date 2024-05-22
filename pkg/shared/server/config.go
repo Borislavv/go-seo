3bcbd3d2d66b7abe935663ac4112baf945256bde
Boris <glazunov2142@icloud.com>
2024-05-22 17:28:55 +0300

    chore: new server config.

---
+++ pkg/shared/server/config.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"github.com/Borislavv/go-seo/internal/shared/helper"
+	"gopkg.in/yaml.v2"
+	"io"
+	"os"
+)
+
+const configFile = "http_server.yaml"
+
+type Config struct {
+	Port                   string `yaml:"port"`                  // example: ":8080"
+	ShutDownTimeoutSeconds int    `yaml:"shutdown_timeout_secs"` // example: 15
+}
+
+func (c *Config) Load() *Config {
+	path, err := helper.ConfigPath(configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = f.Close() }()
+
+	buff, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = yaml.Unmarshal(buff, c); err != nil {
+		panic(err)
+	}
+
+	return c
+}

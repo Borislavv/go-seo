deea3a17c62892900d285b6bc9eb03a385b1941f
Boris <glazunov2142@icloud.com>
2024-05-26 20:42:04 +0300

    feat: improved get controller.

--- internal/pagedata/infrastructure/api/v1/http/controller/get.go
+++ internal/pagedata/infrastructure/api/v1/http/controller/get.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/Borislavv/go-seo/internal/shared/values"
 	"github.com/Borislavv/go-seo/pkg/shared/logger"
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
@@ -9,23 +12,39 @@ import (
 const PagedataGetPath = "/pagedata"
 
 type PagedataGetController struct {
+	ctx    context.Context
 	logger logger.Logger
 }
 
-func NewPagedataGetController(logger logger.Logger) *PagedataGetController {
+func NewPagedataGetController(ctx context.Context, logger logger.Logger) *PagedataGetController {
 	return &PagedataGetController{
+		ctx:    ctx,
 		logger: logger,
 	}
 }
 
 func (c *PagedataGetController) Get(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("application/json")
-	ctx.SetStatusCode(fasthttp.StatusOK)
+	reqCtx, ok := ctx.UserValue(values.CtxKey).(context.Context)
+	if !ok {
+		c.logger.Error(c.ctx, "context.Context is not exists into the fasthttp.RequestCtx "+
+			"(provided default ctx), some part of logs may be lost")
+		reqCtx = c.ctx
+	}
 
-	c.logger.Info("Succeeded response")
+	data := make(map[string]map[string]bool, 1)
+	data["data"] = make(map[string]bool, 1)
+	data["data"]["success"] = true
 
-	if _, err := ctx.WriteString("Success response"); err != nil {
-		c.logger.Error(err)
+	b, err := json.Marshal(data)
+	if err != nil {
+		c.logger.Errorf(c.ctx, "json marshal fail: %v", err.Error())
+	}
+
+	if _, err = ctx.Write(b); err != nil {
+		c.logger.Errorf(reqCtx, "ctx.Write fail: %v", err.Error())
+	} else {
+		ctx.SetStatusCode(fasthttp.StatusOK)
+		c.logger.Info(reqCtx, "http response")
 	}
 }
 

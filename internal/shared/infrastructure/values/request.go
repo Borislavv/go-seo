eae2ce0283cae2d963327f1911ca05c923c1489d
Boris <glazunov2142@icloud.com>
2024-06-09 14:21:40 +0300

    feat: new request values.

---
+++ internal/shared/infrastructure/values/request.go
@@ -0,0 +1,9 @@
+package values
+
+const (
+	RequestStartedAt  = "requestStartedAt"
+	RequestIDKey      = "requestID"
+	RequestIDHeader   = "X-Request-ID"
+	RequestGUIDKey    = "requestGUID"
+	RequestGUIDHeader = "X-Request-GUID"
+)

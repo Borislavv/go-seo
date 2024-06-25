9fab7628229b25482e14079fc5384561ab43078c
Boris <glazunov2142@icloud.com>
2024-06-25 12:51:31 +0300

    feat: moved response writer.

--- pkg/shared/api/http/controller/abstract.go
+++ pkg/shared/api/http/controller/abstract.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"context"
 	"github.com/Borislavv/go-seo/internal/shared/infrastructure/values"
+	"github.com/Borislavv/go-seo/pkg/shared/api/http/response"
 	"github.com/Borislavv/go-seo/pkg/shared/logger"
 )
 
@@ -15,7 +16,7 @@ func NewAbstractController(logger logger.Logger) *Abstract {
 	return &Abstract{logger: logger}
 }
 
-func (c *Abstract) InternalServerError(w WriterOrStringWriter) {
+func (c *Abstract) InternalServerError(w response.WriterOrStringWriter) {
 	if _, err := w.WriteString(values.InternalServerError); err != nil {
 		c.logger.Error(c.ctx, "failed to write response due to "+err.Error())
 		return
